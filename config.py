bd4abfd56cbe6dfaa99818b171dac11ec3238e43
abbasimahan12@gmail.com <abbassimahan12@gmail.com>
2026-07-23 15:16:52 +0330

    modified config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+MODEL_PATH = "Qwen2.5-0.5-instruct"
+MAX_NEW_TOKENS = 512
+TEMPERATURE = 0.7
+TOP_P = 0.9
+
+HISTORY_FILE = Path("chat_history.json")
+
+USER_AVATER = "https://api.dicebear.com/9.x/thumbs/svg?seed=user"
+BOT_AVATAR = "https://api.dicebear.com/9.x/bottts/svg?seed=qwen"
